d87b06b6dbc9df66e9d991b7ede54aee80f1ba6c
MartinECR13 <hee182er@gmail.com>
2023-09-25 22:34:53 -0600

    Probando Métodos

--- Cuenta.java
+++ Cuenta.java
@@ -1,20 +1,102 @@
 package proyecto_bytebank;
-//entidad (objeto) Cuenta{
+
+//*Estando en cualquier instancia podemos retornar a este objeto con el shortcut: ctrl + clickEnMetodo/clickEnCuenta;
+//entidad/objeto Cuenta{
 
 //ATRIBUTOS:
+
 //saldo
 //agencia
 //numero
 //titular
+
+
+//METODOS:
+
+//Depositar
+//Retirar
+//Transferir
+
 //}
+
+
 
 public class Cuenta {
 	
-	
+	//Atributos o campos del objeto Cuenta:
 	double saldo;
 	int agencia;
 	int  numero;
 	String titular;
+	
+	//Metodos:
+	
+	//Hay metodos con parametros : depositar(parametro)  -->recuerda dar nombre significativos a las variables,metodos y parametros;
+	//Y metodos sin parametros:  depositar() 
+	//En JAVA tenemos metodos que retornan valor y los que no retornan valor
+	
+	
+	//Para este ejemplo usaremos un metodo que "no retorna" valor-->void:
+	//por convencion en JAVA, el nombre de los metodos empiezan con minuscula;
+	//public --> es un modificador de acceso;
+	
+	 public void depositar(double valor){
+		 //Para no confundirnos, podemos utilizar this.atributo/campoDelObjetoCuenta ; 
+		 //this. es una referencia que apunta al objeto-> en este caso al objeto Cuenta;
+		this.saldo = this.saldo + valor;
+		//A JAVA le decimos:
+		//Al saldo de "este" objetoCuenta le vas a aumentar el saldo de "este" objetoCuenta que ya existe mas el valor/parametro;
+	    //OJO:
+		//No podemos hacer esto --> this.valor ; por que valor no es un campo del objeto Cuenta, 
+		//valor es un parametro del metodo depositar;
+	 }
+	 
+	 
+	 //Segundo Metodo--retirar()-
+	 
+	 //Ahora nuestro metodo nos "retornara" algo:
+	 //"Exit" o "Error" -->>  por tanto haremos un booleano-->boolean:
+	 
+	 public boolean retirar(double valor){
+		 //Si this.saldo >= al valor que deseo retirar, entonces;
+		 if(this.saldo >= valor) {
+			 System.out.println("Saldo suficiente :)");
+			 //this.saldo = this.saldo -valor;
+			 this.saldo -= valor;
+			 System.out.println("Su saldo actual es: " + this.saldo);
+			 //return --> palabra reservada para retornar valor;
+			 return true;  //Con return con return cortamos este metodo y salimos de  la ejecucion de éste.
+		 }else {
+			 System.out.println("Saldo insuficiente :(" );
+			 System.out.println("Usted quizo retirar: " + valor + " pero, su saldo es de: " + this.saldo );
+			 return false; 
+		 }
+	 }
+	  
+	 //Tercer metodo---- transferir ----
+	 //Para transferir, necesitamos saber a quien transferir(Cuenta cuenta) y cuanto vamos a transferir (double valor);
+	 
+	 public boolean transferir(double valor, Cuenta cuenta){
+		 
+		 if (this.saldo >= valor) {
+			 //this.saldo = this.saldo - valor;
+			 this.saldo -= valor;
+//Puede ser asi-> cuenta.saldo = cuenta.saldo + valor; pero, mejor con nuestro metodo depositar();
+			 cuenta.depositar(valor);
+			 System.out.println(this.titular + " su saldo actual es de: " + this.saldo);
+			 System.out.println("Su transferencia fue exitosa");
+			 return true;
+			 
+		 }//else
+			 System.out.println("Tranferencia NO exitosa, saldo insufiente :(");
+			 return false;
+		 
+		 
+	 }
+	 
+	 
+	 
+	 
 	
 
 }
---
+++ PruebaMetodos.java
@@ -0,0 +1,66 @@
+package proyecto_bytebank;
+
+public class PruebaMetodos {
+	public static void main(String[] args) {
+		Cuenta miCuenta = new Cuenta();
+		Cuenta cuentaDeXimena = new Cuenta();
+		cuentaDeXimena.titular = "Ximena";
+		
+		//Aqui incrementamos 300 a mi saldo:
+		miCuenta.saldo = 300;
+		
+		
+		
+		//Aqui tambien incrementamos (200) a nuestro saldo, solo que ahora es mas chido, pues utilizamos un metodo especifico para ello --> depositar();
+		//En la siguiente linea --> fijate la sintaxis para llamar un metodo:
+		miCuenta.depositar(200);
+		System.out.println(miCuenta.saldo);
+		
+		//Depositamos en la cuenta de Ximena:
+		cuentaDeXimena.depositar(1000);
+		
+		
+		//Aqui retiraremos de miCuenta;
+		//Llamamos al metodo retirar:
+		
+		//Retiramos 100, recuerda que tenemos 300
+		miCuenta.retirar(100);
+		System.out.println();
+		//Intentamos retirar 500, que pasara??---> fijate en la consola;
+		miCuenta.retirar(500);
+		
+		//Ahora vamos a "tranferir" de la cuenta de Jimena a miCuenta:
+		//Al referenciar al objeto miCuenta, tenemos acceso a la informacion contenida dentro de ella;
+		System.out.println();
+		cuentaDeXimena.transferir(400, miCuenta);
+		System.out.println();
+		System.out.println("Checando el saldo de miCuenta, despues de la transferencia de Ximena: " + miCuenta.saldo);
+		
+		
+		//**Vamos a ver la utilidad de metodos que retornan valor:
+		System.out.println();
+		if (cuentaDeXimena.transferir(100, miCuenta)) {   //Porque consigo poner todo ese metodo en la sentencia if?
+    			System.out.println("Transferencia exitosa");//La sentecia if recibe True || False;
+    			                                            //En este caso el metodo transferir "retorna" True;
+    			                                            //Por eso funciona la sentencia if;
+		}
+		
+		System.out.println();
+		//Tambien lo podemos expresar de la siguiente manera:
+		
+		boolean puedeTransferir = cuentaDeXimena.transferir(300, miCuenta);
+		System.out.println();
+		if (puedeTransferir) {
+			System.out.println("Transferencia exitosa");
+		}else {
+			System.out.println("No es posible transferir");
+		}
+		
+		
+		
+		
+		
+		
+	}
+
+}
